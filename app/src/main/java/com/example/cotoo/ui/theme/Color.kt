2f819c58f8f5c36efbdf2b6cf6dd736fd6b20d35
onnil <onni.loponen@gmail.com>
2023-06-26 00:22:08 +0300

    teemat rikki kovakoodattu korjaa myöhemmin

--- app/src/main/java/com/example/cotoo/ui/theme/Color.kt
+++ app/src/main/java/com/example/cotoo/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val lightgreenbg = Color(0xff77ffde)
+val lightgreenhl = Color(0xffc0fff0)
+
+val darkgreenbg = Color(0xff4aa08b)
+val darkgreenhl = Color(0xff356056)
